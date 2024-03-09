96d117133dd2d89b0a10f4a206f141cc0dd52526
azgmohammadd <azgmohammadd@gmail.com>
2024-03-10 00:57:54 +0330

    add tests

--- src/test/java/services/CheckingAccountServiceTest.java
+++ src/test/java/services/CheckingAccountServiceTest.java
@@ -1,2 +1,66 @@
-package services;public class CheckingAccountServiceTest {
+package services;
+
+import com.java.banksystemproject.entities.CheckingAccount;
+import com.java.banksystemproject.services.exception.InsufficientFundsException;
+import com.java.banksystemproject.services.exception.InvalidTransactionException;
+import com.java.banksystemproject.services.impl.CheckingAccountService;
+import com.java.banksystemproject.services.impl.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CheckingAccountServiceTest {
+    private final CheckingAccountService service = new CheckingAccountService(new TransactionService());
+    private CheckingAccount checkingAccount;
+
+    @Before
+    public void startup() {
+        checkingAccount = CheckingAccount.builder().accountNumber("5859831180088659").accountHolderNumber("محمد ازقندی").balance(2000000).overdraftLimit(100000.0).build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void depositNegationValueTest() {
+        service.deposit(checkingAccount, -20000);
+    }
+
+    @Test
+    public void depositPositiveValueTest() {
+        service.deposit(checkingAccount, 200000);
+        assertEquals(checkingAccount.getBalance(), 2200000, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void withdrawNegationValueTest() throws InsufficientFundsException, InvalidTransactionException {
+        service.withdraw(checkingAccount, -20000);
+    }
+
+    @Test(expected = InsufficientFundsException.class)
+    public void withdrawToMuchValueTest() throws InsufficientFundsException, InvalidTransactionException {
+        service.withdraw(checkingAccount, 200000000);
+    }
+
+    @Test(expected = InsufficientFundsException.class)
+    public void withdrawOverDraftLimitTest() throws InsufficientFundsException, InvalidTransactionException {
+        service.withdraw(checkingAccount, 2120000);
+    }
+
+    @Test
+    public void withdrawUnderDraftLimitTest() throws InsufficientFundsException, InvalidTransactionException {
+        service.withdraw(checkingAccount, 2020000);
+        assertEquals(checkingAccount.getBalance(), -20000, 0);
+    }
+
+    @Test
+    public void withdrawEnoughValueTest() throws InsufficientFundsException, InvalidTransactionException {
+        service.withdraw(checkingAccount, 2000000);
+        assertEquals(checkingAccount.getBalance(), 0, 0);
+    }
+
+    @Test
+    public void getBalanceTest() {
+        assertEquals(checkingAccount.getBalance(), 2000000, 0);
+    }
 }
+
--- src/test/java/services/SavingAccountServiceTest.java
+++ src/test/java/services/SavingAccountServiceTest.java
@@ -1,2 +1,63 @@
-package services;public class SavingAccountServiceTest {
+package services;
+
+import com.java.banksystemproject.entities.SavingAccount;
+import com.java.banksystemproject.services.exception.InsufficientFundsException;
+import com.java.banksystemproject.services.exception.InvalidTransactionException;
+import com.java.banksystemproject.services.impl.SavingAccountService;
+import com.java.banksystemproject.services.impl.TransactionService;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class SavingAccountServiceTest {
+    private final SavingAccountService service = new SavingAccountService(new TransactionService());
+    private SavingAccount savingAccount;
+
+    @Before
+    public void startup() {
+        savingAccount = SavingAccount.builder().accountNumber("5859831180088659").accountHolderNumber("محمد ازقندی").balance(2000000).MINIMUM_BALANCE(1000000).build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void depositNegationValueTest() {
+        service.deposit(savingAccount, -20000);
+    }
+
+    @Test
+    public void depositPositiveValueTest() {
+        service.deposit(savingAccount, 200000);
+        assertEquals(savingAccount.getBalance(), 2200000, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void withdrawNegationValueTest() throws InsufficientFundsException, InvalidTransactionException {
+        service.withdraw(savingAccount, -20000);
+    }
+
+    @Test(expected = InvalidTransactionException.class)
+    public void withdrawToMuchValueTest() throws InsufficientFundsException, InvalidTransactionException {
+        service.withdraw(savingAccount, 200000000);
+    }
+
+    @Test(expected = InvalidTransactionException.class)
+    public void withdrawOverMinimumTest() throws InsufficientFundsException, InvalidTransactionException {
+        service.withdraw(savingAccount, 1950000);
+    }
+
+    @Test
+    public void withdrawUnderMinimumTest() throws InsufficientFundsException, InvalidTransactionException {
+        service.withdraw(savingAccount, 200000);
+        assertEquals(savingAccount.getBalance(), 1800000, 0);
+    }
+
+    @Test
+    public void getBalanceTest() {
+        assertEquals(savingAccount.getBalance(), 2000000, 0);
+    }
+
 }
+
