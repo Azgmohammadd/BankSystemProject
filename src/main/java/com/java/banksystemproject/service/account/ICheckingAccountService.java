c65888ad8d43666b72e765206dba8208ed31d42f
h.ebrahimi <h.ebrahimi@fanap-infra.ir>
2024-03-10 16:17:32 +0330

    V2

--- src/main/java/com/java/banksystemproject/service/account/ICheckingAccountService.java
+++
@@ -1,7 +0,0 @@
-package com.java.banksystemproject.service.account;
-
-import com.java.banksystemproject.model.account.CheckingAccount;
-
-public interface ICheckingAccountService extends IBankAccountService {
-    void withdraw(CheckingAccount account, double amount);
-}
