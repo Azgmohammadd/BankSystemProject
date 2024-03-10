66db97a2530a6756652c407ad58e76da7eb177a1
h.ebrahimi <h.ebrahimi@fanap-infra.ir>
2024-03-10 10:00:42 +0330

    V2

--- src/main/java/com/java/banksystemproject/dao/impl/BankDao.java
+++ src/main/java/com/java/banksystemproject/dao/impl/BankDao.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @Data
 @RequiredArgsConstructor
 public class BankDao implements Dao<BankAccount>, Serializable {
+
     @Serial
     private static final long serialVersionUID = 1L;
     private final Map<String, BankAccount> accountMap;
