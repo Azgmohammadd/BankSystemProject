169024645a8a136959839955e795047b319891d3
azgmohammadd <azgmohammadd@gmail.com>
2024-03-12 01:54:41 +0330

    add JDBCUtil and update PropertiesFileService.java

--- src/main/java/com/java/banksystemproject/util/impl/PropertiesFileUtil.java
+++ src/main/java/com/java/banksystemproject/util/impl/PropertiesFileUtil.java
@@ -1,17 +1,15 @@
 package com.java.banksystemproject.util.impl;
 
-import com.java.banksystemproject.util.IFileService;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-public class PropertiesFileService {
-    public Properties readPropertiesFile() {
+public class PropertiesFileUtil {
+    public static Properties getProperties(String filePath) {
         Properties prop = new Properties();
 
-        try (InputStream input = new FileInputStream("src/main/resources/application.properties")) {
+        try (InputStream input = new FileInputStream(filePath)) {
             prop.load(input);
 
         } catch (IOException ex) {
