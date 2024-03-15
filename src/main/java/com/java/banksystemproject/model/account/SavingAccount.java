3f3670c9c957ffa640e6ff7337827197a3f1dd17
azgmohammadd <azgmohammadd@gmail.com>
2024-03-16 01:39:37 +0330

    update entities
    add jpa annotations

--- src/main/java/com/java/banksystemproject/model/account/SavingAccount.java
+++ src/main/java/com/java/banksystemproject/model/account/SavingAccount.java
@@ -4,16 +4,25 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import java.io.Serializable;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @SuperBuilder
+@Entity
+@DiscriminatorValue("Saving")
 public class SavingAccount extends BankAccount implements Serializable {
-    private float MonthlyInterestRate;
-    private double MINIMUM_BALANCE;
-    private double minimumBalanceInMonth;
+    @Column(name = "monthly_interest_rate")
+    private float monthlyInterestRate;
 
+    @Column(name = "minimum_balance")
+    private double minimumBalance;
+
+    @Column(name = "minimum_balance_in_month")
+    private double minimumBalanceInMonth;
     public SavingAccount(String accountNumber, String accountHolderNumber, float interestRate) {
         super(accountNumber, accountHolderNumber);
         this.setMonthlyInterestRate(interestRate);
