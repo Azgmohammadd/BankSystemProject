64dc3a9ce772f34ea46ab6b37af61f179ba9c27b
root <ebrahimi.hashem@gmail.com>
2024-03-16 13:22:56 +0330

    merge

--- src/main/java/com/java/banksystemproject/service/account/factory/SavingAccountServiceFactory.java
+++ src/main/java/com/java/banksystemproject/service/account/factory/SavingAccountServiceFactory.java
@@ -1,12 +1,23 @@
 package com.java.banksystemproject.service.account.factory;
 
+import com.java.banksystemproject.dao.impl.BankAccountDao;
 import com.java.banksystemproject.dao.impl.JDBC.BankAccountDaoJDBC;
 import com.java.banksystemproject.dao.impl.JDBC.TransactionDaoJDBC;
+import com.java.banksystemproject.dao.impl.TransactionDao;
 import com.java.banksystemproject.service.account.ISavingAccountService;
 import com.java.banksystemproject.service.account.impl.SavingAccountService;
 import com.java.banksystemproject.service.impl.TransactionService;
 
 public class SavingAccountServiceFactory {
+
+    public ISavingAccountService get(){
+        return new SavingAccountService(
+                new TransactionService(),
+                new BankAccountDao(),
+                new TransactionDao()
+        );
+    }
+
     public ISavingAccountService getJDBC(){
         return new SavingAccountService(
                 new TransactionService(),
