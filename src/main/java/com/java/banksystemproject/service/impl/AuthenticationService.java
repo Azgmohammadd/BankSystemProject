4fafd42257b925fce63799ddc280c4bf90eb5723
azgmohammadd <azgmohammadd@gmail.com>
2024-04-02 05:30:58 +0330

    add Authentication Service

---
+++ src/main/java/com/java/banksystemproject/service/impl/AuthenticationService.java
@@ -0,0 +1,83 @@
+package com.java.banksystemproject.service.impl;
+
+import com.java.banksystemproject.dao.IBankUserDao;
+import com.java.banksystemproject.dao.ITokenDao;
+import com.java.banksystemproject.model.authentication.AuthenticationRequest;
+import com.java.banksystemproject.model.authentication.AuthenticationResponse;
+import com.java.banksystemproject.model.BankUser;
+import com.java.banksystemproject.model.authentication.RegisterRequest;
+import com.java.banksystemproject.service.IAuthenticationService;
+import com.java.banksystemproject.util.impl.JwtUtil;
+import com.java.banksystemproject.util.impl.PasswordEncoder;
+import lombok.RequiredArgsConstructor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@RequiredArgsConstructor
+public class AuthenticationService implements IAuthenticationService {
+    private final IBankUserDao userDao;
+    private final ITokenDao tokenDao;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = BankUser.builder()
+                .username(request.getUsername())
+                .password(PasswordEncoder.encodePassword(request.getPassword()))
+                .role(request.getRole())
+                //TODO: add bankAccounts
+                .build();
+
+        userDao.save(user);
+
+        String accessToken = JwtUtil.generateToken(user);
+        String refreshToken = JwtUtil.generateRefreshToken(user);
+
+        return AuthenticationResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .responseMessage("registered")
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        BankUser user = userDao.get(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found")
+        );
+
+        if (!PasswordEncoder.matches(request.getPassword(), user.getPassword())){
+            throw new RuntimeException("Invalid password");
+        }
+
+        String accessToken = JwtUtil.generateToken(user);
+        String refreshToken = JwtUtil.generateRefreshToken(user);
+
+        return AuthenticationResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .responseMessage("authenticated")
+                .build();
+    }
+
+    public AuthenticationResponse logout(HttpServletRequest request, HttpServletResponse response) {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return null;
+        }
+
+        jwt = authHeader.substring(7);
+        var stored_token = tokenDao.findByToken(jwt).orElse(null);
+
+        if (stored_token != null) {
+            stored_token.setExpired(true);
+            stored_token.setRevoked(true);
+
+            tokenDao.save(stored_token);
+        }
+        return AuthenticationResponse.builder()
+                .responseMessage("logged_out")
+                .build();
+    }
+}
--- src/main/java/com/java/banksystemproject/service/impl/BankUserService.java
+++ src/main/java/com/java/banksystemproject/service/impl/BankUserService.java
@@ -17,23 +17,23 @@ public class BankUserService implements IBankUserService {
 
     @Override
     public void register(BankUser user) {
-        Optional<BankUser> entity = bankUserDao.get(user.getUserName());
+        Optional<BankUser> entity = bankUserDao.get(user.getUsername());
 
         if(entity.isPresent())
             throw new IllegalArgumentException(ExceptionMessageCodes.BSS_USER_NAME_ALREADY_EXIST);
 
-        user.setPassWord(BCrypt.hashpw(user.getPassWord(), BCrypt.gensalt()));
+        user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
         bankUserDao.save(user);
     }
 
     @Override
     public BankUser authenticate(BankUser user){
-        Optional<BankUser> entity = bankUserDao.get(user.getUserName());
+        Optional<BankUser> entity = bankUserDao.get(user.getUsername());
 
         if(entity.isEmpty())
             throw new IllegalArgumentException(ExceptionMessageCodes.BSS_USER_NOT_EXIST);
 
-        if(BCrypt.checkpw(user.getPassWord(), entity.get().getPassWord()))
+        if(BCrypt.checkpw(user.getPassword(), entity.get().getPassword()))
             return entity.get();
 
         throw new IllegalArgumentException(ExceptionMessageCodes.BSS_USER_NOT_EXIST);
