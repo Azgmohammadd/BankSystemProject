4646803f445db9405c720290d26238f9b8f9b90c
azgmohammadd <azgmohammadd@gmail.com>
2024-04-04 04:43:12 +0330

    update enocdePassword

--- src/main/java/com/java/banksystemproject/util/impl/PasswordEncoder.java
+++ src/main/java/com/java/banksystemproject/util/impl/PasswordEncoder.java
@@ -1,12 +1,24 @@
 package com.java.banksystemproject.util.impl;
 
-import org.mindrot.jbcrypt.BCrypt;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class PasswordEncoder {
     public static String encodePassword(String password) {
-        return BCrypt.hashpw(password, BCrypt.gensalt());
-    }
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
 
+            md.update(password.getBytes());
+
+            byte[] hashedBytes = md.digest();
+
+            return Base64.getEncoder().encodeToString(hashedBytes);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
     public static boolean matches(String plainPassword, String hashedPassword) {
         String hashedPlainPassword = encodePassword(plainPassword);
         return hashedPassword.equals(hashedPlainPassword);
